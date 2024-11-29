8a43b258344c530ac7c8d525773c7ef86e4d4e1d
Adria Hernandez <ahernandezlasc@gmail.com>
2024-11-29 15:45:29 -0600

    tech card component is created and styled, transitions still in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import TechCard from './TechCard'
+import './App.css'
+import './TechCard.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <TechCard />
+      <TechCard techName='Type Script' image='https://coryrylan.com/assets/images/posts/types/typescript.png'/>
+      <TechCard techName='Java Script' image='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/640px-Unofficial_JavaScript_logo_2.svg.png'/>
     </>
   )
 }
--- src/TechCard.tsx
+++ src/TechCard.tsx
@@ -1,7 +1,29 @@
-function TechCard(){
+import { useState } from "react";
+
+function TechCard({techName, image}: {techName: string, image: string}){
+    const [visibility,setVisibility] = useState('none');
+
+    function handleMouseEnter() {
+        setVisibility('block')
+    }// handleMouseEnter
+
+    function handleMouseLeave() {
+        setVisibility('none')
+    }// handleMouseLeave
+
     return (
-        <h1>hola</h1>
+        <div className="tech-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <figure>
+                <img src={image} alt={techName} />
+            </figure>
+            <div className="tech-name-container" style={{display: visibility}}>
+                <div className="blur-container"></div>
+                <div className="tech-name">
+                    <h3>{techName}</h3>
+                </div>
+            </div>
+        </div>
     );
-}
+} // TechCard
 
 export default TechCard;
