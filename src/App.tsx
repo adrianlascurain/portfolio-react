1524f354abf9d9eac477f94888fa792f75069ed2
Adria Hernandez <ahernandezlasc@gmail.com>
2024-11-29 20:52:47 -0600

    Tech Card opacity when hover is adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import TechCard from './TechCard'
+// import TechCard from './TechCard'
 import './App.css'
 import './TechCard.css'
 
@@ -30,8 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <TechCard techName='Type Script' image='https://coryrylan.com/assets/images/posts/types/typescript.png'/>
-      <TechCard techName='Java Script' image='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/640px-Unofficial_JavaScript_logo_2.svg.png'/>
+      
     </>
   )
 }
--- src/TechCard.tsx
+++ src/TechCard.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 
 function TechCard({techName, image}: {techName: string, image: string}){
     const [visibility,setVisibility] = useState('none');
+    const [opacityValue,setOpacityValue] = useState(0.6);
 
     function handleMouseEnter() {
-        setVisibility('block')
+        setVisibility('block');
+        setOpacityValue(0.7);
     }// handleMouseEnter
 
     function handleMouseLeave() {
-        setVisibility('none')
+        setVisibility('none');
+        setOpacityValue(0);
     }// handleMouseLeave
 
     return (
@@ -17,7 +20,7 @@ function TechCard({techName, image}: {techName: string, image: string}){
                 <img src={image} alt={techName} />
             </figure>
             <div className="tech-name-container" style={{display: visibility}}>
-                <div className="blur-container"></div>
+                <div className="blur-container" style={{opacity: opacityValue}}></div>
                 <div className="tech-name">
                     <h3>{techName}</h3>
                 </div>
